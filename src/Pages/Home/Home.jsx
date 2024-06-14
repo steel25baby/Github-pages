61e9c8f0bf469ed5bb3d7cde6e53db3b920e3f0b
steel25baby <bridgetgitonga73@gmail.com>
2024-06-14 17:38:28 +0300

    second commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Profile from './Profile'
+import "./Profile.css"
 
 const Home = () => {
   return (
--- src/Pages/Home/Profile.jsx
+++ src/Pages/Home/Profile.jsx
@@ -1,8 +1,64 @@
 import React from 'react'
+import profile from "../../assets/profile.jpeg"
+import { MdLocationOn } from "react-icons/md";
+import { GiTeacher } from "react-icons/gi";
+import { RiGitRepositoryFill } from "react-icons/ri";
+import { MdGroups } from "react-icons/md";
+import { VscRepoForked } from "react-icons/vsc";
+import { FaStar } from "react-icons/fa";
+import { RiExternalLinkFill } from "react-icons/ri";
+
+const Info = ({ Name, icon  }) => {
+    return (
+         
+      <div className="Info">
+        <div className="Profile-icon-wrapper">{icon} 
+        <p>{Name}</p>
+        </div>
+      </div>
+    );
+  };
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <>
+    <section className='HeroSection'>
+        <div className='HomePage'>
+            <div className='ProfileDetails'>
+                <div className='ProfilePic'>
+                    <img src={profile} alt="" />
+                </div>
+                <div className='GithubDetails'> 
+                  <h2>Bridget Gitonga</h2>
+
+                    <h4>GitHub</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur <br/> dipisicing elit. Architecto, aliquam.</p>
+                    <a  href=""><RiExternalLinkFill /> View on GitHub</a>
+                </div>
+                <div className='GithubInfo'> 
+                        <Info icon={<MdLocationOn/>} Name="Location"/>
+                        <Info icon={<GiTeacher/>} Name="TEACH2GIVE"/>
+                        <Info icon={<RiGitRepositoryFill/>} Name="Repositories"/>
+                        <Info icon={<MdGroups/>} Name="Followers"/>
+                        <Info icon={<MdGroups/>} Name="Following"/>
+                </div>
+            </div>
+            <div className='Repositories'>
+              <div className='RepositoryCard'>
+                <h2>Air BnB Close</h2>
+                <p>alx backend user data</p>
+                <div className='RepositoryCardIcon'>
+                  <Info icon={<VscRepoForked/>} Name="0 forks"/>
+                  <Info icon={<FaStar/>} Name="0 stars"/>
+
+                </div>
+              </div>
+            </div>
+            
+            
+        </div>
+    </section>
+    </>
   )
 }
 
