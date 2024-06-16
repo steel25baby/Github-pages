77ec0d516a72c257855187ce3581592e4a6aaae0
steel25baby <bridgetgitonga73@gmail.com>
2024-06-16 18:24:07 +0300

    fetched the first api

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,46 +1,53 @@
-import React from 'react'
-import "./Header.css"
-import { useState } from 'react'
-import Profile from '../../Pages/Home/Profile'
+import React, { useState } from 'react';
+import Profile from '../../Pages/Home/Profile';
 
 const Header = () => {
-  const [userdata, setuserdata]=useState("steel25baby")
-  const [userdatafetch, setuserdatafetch]=useState(null)
-  const [error, seterror]=useState(false)
-  const HandleGituser = async(e)=>{
-    e.preventDefault()
-    setuserdatafetch(null)
+  const [userdata, setuserdata] = useState("steel25baby");
+  const [userdatafetch, setuserdatafetch] = useState([]);
+  const [error, seterror] = useState(false);
+
+  const HandleGituser = async (e) => {
+    e.preventDefault();
+    setuserdatafetch([]);
+    seterror(false);
     try {
-      const responceuser=await fetch(`https://api.github.com/users/${userdata}`)
-console.log (responceuser)
-if (responceuser.ok===true){
-  const datauser=await responceuser.json()
-  console.log(datauser)
-  setuserdatafetch(datauser)
-}
+      const responceuser = await fetch(`https://api.github.com/users/${userdata}`);
+      console.log(responceuser);
+      if (responceuser.ok) {
+        const datauser = await responceuser.json();
+        console.log(datauser);
+        setuserdatafetch(datauser);
+      } else {
+        seterror("User not found");
+      }
     } catch (error) {
-      seterror("there was an error")
+      seterror("There was an error");
     }
-  }
+  };
+
   return (
     <section className='NavBar'>
-        <div className='Heading'>
-            <h1>GITHUB FINDER</h1>
-            <p>By <a href="">Bridget Gitonga</a></p>
-            <div className='HeaderLabel'>
-                <label htmlFor="username"></label>
-                <input type ="text"
-                id ='username'
-                placeholder ='enter a username'
-                 />
-                 <button  onClick={HandleGituser} className='HeaderButton'>search</button>
-            </div>
+      <div className='Heading'>
+        <h1>GITHUB FINDER</h1>
+        <p>By <a href="">Bridget Gitonga</a></p>
+        <div className='HeaderLabel'>
+          <label htmlFor="username"></label>
+          <input
+            type="text"
+            id='username'
+            placeholder='Enter a username'
+            value={userdata}
+            onChange={(e) => setuserdata(e.target.value)}
+          />
+          <button onClick={HandleGituser} className='HeaderButton'>Search</button>
         </div>
-        <div>
-          <Profile />
-        </div>
+      </div>
+      {error && <p className="error">{error}</p>}
+      <div>
+        <Profile userdatafetch={userdatafetch} />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/Pages/Home/Profile.jsx
+++ src/Pages/Home/Profile.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import profile from "../../assets/profile.jpeg"
+import React from 'react';
+import profile from "../../assets/profile.jpeg";
 import { MdLocationOn } from "react-icons/md";
 import { GiTeacher } from "react-icons/gi";
 import { RiGitRepositoryFill } from "react-icons/ri";
@@ -8,58 +8,50 @@ import { VscRepoForked } from "react-icons/vsc";
 import { FaStar } from "react-icons/fa";
 import { RiExternalLinkFill } from "react-icons/ri";
 
-const Info = ({ Name, icon  }) => {
-    return (
-         
-      <div className="Info">
-        <div className="Profile-icon-wrapper">{icon} 
+const Info = ({ Name, icon }) => {
+  return (
+    <div className="Info">
+      <div className="Profile-icon-wrapper">{icon}
         <p>{Name}</p>
+      </div>
+    </div>
+  );
+};
+
+const Profile = ({ userdatafetch }) => {
+  if (!userdatafetch || Object.keys(userdatafetch).length === 0) {
+    return <p>No user data found</p>;
+  }
+
+  return (
+    <section className='HeroSection'>
+      <div className='HomePage'>
+        <div className='ProfileDetails'>
+          <div className='ProfilePic'>
+            <img src={userdatafetch.avatar_url || profile} alt="Profile" />
+          </div>
+          <div className='GithubDetails'>
+            <h2>{userdatafetch.name || "No Name"}</h2>
+            <h4>GitHub</h4>
+            <p>{userdatafetch.bio || "No bio available"}</p>
+            <a href={userdatafetch.html_url} target="_blank" rel="noopener noreferrer">
+              <RiExternalLinkFill /> View on GitHub
+            </a>
+          </div>
+          <div className='GithubInfo'>
+            <Info icon={<MdLocationOn />} Name={userdatafetch.location || "No location"} />
+            <Info icon={<GiTeacher />} Name={userdatafetch.company || "No company"} />
+            <Info icon={<RiGitRepositoryFill />} Name={`${userdatafetch.public_repos} Repositories`} />
+            <Info icon={<MdGroups />} Name={`${userdatafetch.followers} Followers`} />
+            <Info icon={<MdGroups />} Name={`${userdatafetch.following} Following`} />
+          </div>
+        </div>
+        <div className='Repositories'>
+          {/* Add logic to display repositories if needed */}
         </div>
       </div>
-    );
-  };
-
-const Profile = ({userdatafetch}) => {
-  return (
-    <>
-    <section className='HeroSection'>
-        <div className='HomePage'>
-            <div className='ProfileDetails'>
-                <div className='ProfilePic'>
-                    <img src={profile} alt="" />
-                </div>
-                <div className='GithubDetails'> 
-                  <h2>{userdatafetch.name}</h2>
-
-                    <h4>GitHub</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur <br/> dipisicing elit. Architecto, aliquam.</p>
-                    <a  href=""><RiExternalLinkFill /> View on GitHub</a>
-                </div>
-                <div className='GithubInfo'> 
-                        <Info icon={<MdLocationOn/>} Name="Location"/>
-                        <Info icon={<GiTeacher/>} Name="TEACH2GIVE"/>
-                        <Info icon={<RiGitRepositoryFill/>} Name="Repositories"/>
-                        <Info icon={<MdGroups/>} Name="Followers"/>
-                        <Info icon={<MdGroups/>} Name="Following"/>
-                </div>
-            </div>
-            <div className='Repositories'>
-              <div className='RepositoryCard'>
-                <h2>Air BnB Close</h2>
-                <p>alx backend user data</p>
-                <div className='RepositoryCardIcon'>
-                  <Info icon={<VscRepoForked/>} Name="0 forks"/>
-                  <Info icon={<FaStar/>} Name="0 stars"/>
-
-                </div>
-              </div>
-            </div>
-            
-            
-        </div>
     </section>
-    </>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
